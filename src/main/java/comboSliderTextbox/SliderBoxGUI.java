361d7e1603ddca76e2a15ea919d89c3b133d62a4
kapoorlab <kapoorconsciousness@gmail.com>
2018-06-04 15:09:16 +0200

    Slider and textbox combo

--- src/main/java/comboSliderTextbox/SliderBoxGUI.java
+++ src/main/java/comboSliderTextbox/SliderBoxGUI.java
@@ -12,35 +12,38 @@ import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
 
+
 public class SliderBoxGUI {
 
 	
 	final String string;
-	final int valueDimension;
+	final float valueDimension;
+	final float valueMax;
 	final int scrollbarSize;
+	final JScrollBar valueslider;
+	final TextField inputFieldvalue;
+	final Label valueText;
 	
-	public SliderBoxGUI(final String string, final int scrollbarSize, final int valueDimension) {
+	public SliderBoxGUI(final String string, JScrollBar valueslider, TextField inputFieldvalue, Label valueText,  final int scrollbarSize, final float valueDimension, final float valueMax) {
 		
 		this.string  = string;
 		this.scrollbarSize = scrollbarSize;
 		this.valueDimension = valueDimension;
-		
+		this.valueMax = valueMax;
+		this.valueslider = valueslider;
+		this.inputFieldvalue = inputFieldvalue;
+		this.valueText = valueText;
 	}
 	
 	
 	
-	public void BuildDisplay() {
+	public JPanel BuildDisplay() {
 	
-	Label valueText = new Label(this.string + 1, Label.CENTER);
-	String valuestring = string;
-	JScrollBar valueslider = new JScrollBar(Scrollbar.HORIZONTAL, 0, 10, 0,
-			scrollbarSize + 10);
+
 	JPanel combosliderbox = new JPanel();
-	TextField inputFieldT = new TextField(5);
-	inputFieldT.setText(Integer.toString(valueDimension));
+
 	layoutManager.Setlayout.LayoutSetter(combosliderbox);
 	
-	Border valueborder = new CompoundBorder(new TitledBorder("Select value"), new EmptyBorder(layoutManager.Setlayout.insets));
 	
 
 	combosliderbox.add(valueText, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
@@ -49,10 +52,13 @@ public class SliderBoxGUI {
 	combosliderbox.add(valueslider, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
 			GridBagConstraints.HORIZONTAL, layoutManager.Setlayout.insets, 0, 0));
 
-	combosliderbox.add(inputFieldT, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
+	combosliderbox.add(inputFieldvalue, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.WEST,
 			GridBagConstraints.HORIZONTAL, layoutManager.Setlayout.insets, 0, 0));
 
-	combosliderbox.setBorder(valueborder);
+	
+	
+	return combosliderbox;
+
 	
 	
 	}
